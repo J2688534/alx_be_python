01ac27deaabc94d35af17d00a14f823bae86ec9a
Obeng Josiah <josiahobeng87@gmail.com>
2025-06-06 13:42:18 +0000

    pattern

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,17 @@
+size = int(input("Enter the size of the pattern: "))
+row = 0
+while row < size:
+    # draw a row of stars here
+    row += 1
+
+
+while row < size:
+    # we'll draw one row of asterisks here
+    row += 1  # Don't forget to increase row after each iteration
+
+
+
+
+for col in range(size):
+    print("*", end="")
+print()
